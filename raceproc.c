306cf754599bddfd26cb2e93b671c34352e2462a
Hieu Quang <hquang@unm.edu>
2022-10-31 13:42:24 -0600

    Update: Functional prototype, needs comments

--- raceproc.c
+++ raceproc.c
@@ -13,20 +13,15 @@ typedef struct moneymcpherson {
     int balance[2];
 } Bank;
 
-struct semaphores {
-    sem_t mtx;
-};
-
-sem_t *test;
+sem_t *mtx;
 struct moneymcpherson *x; 
-struct semaphores *s;
 
 void* MakeTransactions() {
     int i, j, tmp1, tmp2, rint;
     double dummy;
 
     for (i = 0; i < 100; i++) {
-        sem_wait(test);
+        sem_wait(mtx);
         rint = (rand() % 30) - 15;
         if ((((tmp1 = x->balance[0]) + rint) >= 0) &&
                 (((tmp2 = x->balance[1]) - rint) >= 0)) {
@@ -36,22 +31,16 @@ void* MakeTransactions() {
             }
             x->balance[1] = tmp2 - rint;
         }
-        sem_post(test);
+        sem_post(mtx);
     }
     return NULL;
 }
 
-int main (int argc, char** argv) {
-    pid_t pid;
-    srand(getpid());
-    char* shm;
-    size_t BANKSZ = sizeof(Bank);
-    size_t SEMSZ = sizeof(sem_t);
-    size_t MEMSZ = (BANKSZ + SEMSZ); 
-    int shmid1, shmid2;
+/* ----- Memory segment for moneymcpherson ----- */
+void createMoneyMemSpace() {
+    int shmid1;
+    key_t key1 = ftok("shmfile1",12);
 
-    /* ----- Memory segment for moneymcpherson ----- */
-    key_t key1 = ftok("shmfile1",12343);
     // Create memory space segment
     if ((shmid1 = shmget(key1, 32, IPC_CREAT | 0666)) < 0) {
         perror("shmget1");
@@ -64,12 +53,16 @@ int main (int argc, char** argv) {
         exit(1);
     }
 
-    // Setting up the balance since we are NOT using Bank
+    // Setting up the balance since we are NOT using Bank variable
     x->balance[0] = 100;
     x->balance[1] = 100;
+}
 
-    /* ----- Memory segment for semaphores ----- */
+/* ----- Memory segment for semaphores ----- */
+void createSmphrMemSpace() {
+    int shmid2;
     key_t key2 = ftok("shmfile2",87);
+
     // Create memory space segment
     if ((shmid2 = shmget(key2, 32, IPC_CREAT | 0666)) < 0) {
         perror("shmget2");
@@ -77,17 +70,25 @@ int main (int argc, char** argv) {
     }
 
     // Attach it to our object to use
-    if ((test = shmat(shmid2, NULL, 0)) == (sem_t*) -1) {
+    if ((mtx = shmat(shmid2, NULL, 0)) == (sem_t*) -1) {
         perror("shmat");
         exit(1);
     }
 
-    test = (sem_t*) (test + 1);
-    sem_init(test, 1, 1);
+    mtx = (sem_t*) (mtx + 1);
+}
+
+int main (int argc, char** argv) {
+    pid_t pid;
+    srand(getpid());
+
+    createMoneyMemSpace(); 
+    createSmphrMemSpace();
+    
+    sem_init(mtx, 1, 1);
 
     /* ----- Forking ----- */
     for (int i = 0; i < NUM; i++) { 
-        printf("i = %d\n", i);
         printf("Init Balances A:%d + B:%d ==> %d!\n", 
                 x->balance[0], x->balance[1], 
                 x->balance[0] + x->balance[1]);
@@ -111,25 +112,16 @@ int main (int argc, char** argv) {
             default:
                 {
                     MakeTransactions();
-
-                    sem_wait(test);
-                                          fflush(stdout);
-                       //wait(NULL);
-                    sem_post(test);
+                    wait(NULL);
                     break;
                 }
-            printf("Let's check the balances A:%d + B:%d ==> %d ?= 200\n", 
-                      x->balance[0], x->balance[1], 
-                      x->balance[0] + x->balance[1]);
-
         }
-        /*
-        printf("Let's check the balances A:%d + B:%d ==> %d ?= 200\n", 
+        printf("Let's check the balances A:%d + B:%d ==> %d ?= 200\n\n", 
                 x->balance[0], x->balance[1], 
                 x->balance[0] + x->balance[1]);
-                */
     }
-    sem_destroy(test);
+
+    sem_destroy(mtx);
 
     return 0;
 }
